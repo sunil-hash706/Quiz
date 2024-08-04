6c9cd08063aa4ed888796b0503001d974c9078b2
Sunil Kumar <77500332+sunil-hash706@users.noreply.github.com>
2024-08-04 13:05:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 // require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 
 // Middleware
 // app.use(cors({
