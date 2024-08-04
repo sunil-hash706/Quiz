8e58cd2e49bce5cb141f5a75e6180ded5311826a
Sunil Kumar <77500332+sunil-hash706@users.noreply.github.com>
2024-08-04 13:04:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config(); // Load environment variables from .env file
+// require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 5000;
