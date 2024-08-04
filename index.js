fcf56dea874bf5b6566399ca122a318caa344475
Sunil Kumar <77500332+sunil-hash706@users.noreply.github.com>
2024-08-04 11:36:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://quiz-client-ten.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
